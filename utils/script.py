72777249db28170e4cfea0a3067eaf60dd026da4
Royaldeep1 <120001510+Royaldeep1@users.noreply.github.com>
2023-04-21 19:02:55 +0530

    Update script.py

--- utils/script.py
+++ utils/script.py
@@ -1,26 +1,14 @@
 class script(object):
-    START = """**Hello {} 🤟**
+    START = """**Hey {} 😃**
    
-I am **Find Post Bot**.I am best Channel Link Search Bot! 
-I Will filter your channel posts automatically and send it in your group chat when Someone search it."""
+I Am **Nobita Filter Bot**.I am best Channel Post Link Search Bot! I Will filter your channel posts links automatically and send it in your group chat when Someone search it."""
 
-    HELP = """To Use me In A Group
-
-- Add me in your group & channel with all permissions. 
-- Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @GreyMatter_Bots.
-- After verification send /connect YourChannelID
-- Example : /connect -100xxxxxxxxxx
-- Done ✅. <b><I>Enjoy 💜❤</I></b> )
-
-Remove a Channel with - /disconnect-100xxxxxxxxxxx
-this will help you to remove a indexed channel from your group.
-
-Get connected channels list with - /connections"""
+    HELP = """<b>How To Use Nobita Filter❓\n\n🍓 Add me in your group & channel with all permissions. \n\n🍓 Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @RoyalDwip.\n\n🍓 After verification send /connect YourChannel ID! \n- Example : /connect -100xxxxxxxxxx\n- Done ✅\n\nNow Earn With Nobita Style 😁\n\n🍓 Remove a Channel With : /disconnect  <CHANNEL_ID> \n\nExample 👉 /disconnect -100xxxxxxxxxx\n\n🍓 This will help you to remove a indexed channel from your group.\n\n🍓 Get connected channels list with - /connections.\n\nHope You Will Enjoy With Our Bot ❤</b>"""
 
     ABOUT = """Developed By @CyniteBackup
 
 ✯ Mʏ Nᴀᴍᴇ:  {}
-✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/CyniteOfficial'>Harman</a>
+✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/RoyalDwip'>Rᴏʏᴀʟ Dᴡɪᴘ</a>
 ✯ Lᴀɴɢᴜᴀɢᴇ: <a href='https://www.python.org'>Pʏᴛʜᴏɴ 3</a>
 ✯ DᴀᴛᴀBᴀsᴇ: <a href='https://cloud.mongodb.com/'>MᴏɴɢᴏDB</a>
 ✯ Bᴏᴛ Sᴇʀᴠᴇʀ: : <a href='https://heroku.com'>Heroku</a>"""
