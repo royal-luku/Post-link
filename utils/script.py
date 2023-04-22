a2b3edd5144a2b4b8d7d8f4e395a0f04ac9dafec
Royaldeep1 <120001510+Royaldeep1@users.noreply.github.com>
2023-04-22 06:21:38 +0530

    Update script.py

--- utils/script.py
+++ utils/script.py
@@ -1,11 +1,11 @@
 class script(object):
     START = """**Hey {} 😃**
    
-<b>I Am <i>Nobita Filter Bot</i>.I am best Channel Post Link Search Bot! I Will filter your channel posts links automatically and send it in your group chat when Someone search it.\n\nMade With By @RoyalDwip ❤<b>"""
+<b>I Am <i>Nobita Filter Bot</i>.\n\nI am best Channel Post Link Search Bot! I Will filter your channel posts links automatically and send it in your group chat when Someone search it.\n\nMade With By @RoyalDwip ❤</b>"""
 
     HELP = """<b>How To Use Nobita Filter Bot❓\n\n🍓 Add me in your group & channel with all permissions. \n\n🍓 Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request 👉 @RoyalDwip.\n\n🍓 After verification send /connect Your Channel ID! \n- Example : /connect -100xxxxxxxxxx\n- Done ✅\n\nNow Earn With Nobita Style 😁\n\n🍓 Remove Your Connected Channel With : /disconnect  <CHANNEL_ID> \n\nExample 👉 /disconnect -100xxxxxxxxxx\n\n🍓 This will help you to remove a indexed channel from your group.\n\n🍓 Get connected channels list with - /connections.\n\nHope You Will Enjoy With Our Bot ❤ \nPlease Share Our Bot With Your Friends 👍</b>"""
 
-    ABOUT = """<b>Developed By @WomBackup\n\nORG Group 👉 @WomRequest.</b>
+    ABOUT = """<b>Developed By @WomBackup\nMy Group - @WomRequest.</b>
 
 ✯ Mʏ Nᴀᴍᴇ:  {}
 ✯ Cʀᴇᴀᴛᴏʀ: <a href='https://t.me/RoyalDwip'>Rᴏʏᴀʟ Dᴡɪᴘ</a>
